256a58de6c1d35cba5301256fef48711ad9e421c
ronval <ronald.m.valencia@gmail.com>
2017-02-04 14:13:55 +1100

    removed config code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,9 +86,4 @@ Rails.application.configure do
   
   #Variant setting for react-rails
   config.react.variant = :production
-  config.react.jsx_transform_options = {
-    blacklist: ['spec.functionName', 'validation.react', 'strict'], # default options
-    optional: ["transformerName"],  # pass extra babel options
-    whitelist: ["useStrict"] # even more options
-  }
 end
