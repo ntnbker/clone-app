7207655202c4aaca2490ff3c59ab307db8b44973
ronval <ronald.m.valencia@gmail.com>
2017-03-02 11:39:22 +1100

    fixed typo

--- app/controllers/search_maintenance_requests_controller.rb
+++ app/controllers/search_maintenance_requests_controller.rb
@@ -3,7 +3,7 @@ class SearchMaintenanceRequestsController < ApplicationController
   def search_maintenance_request
     binding.pry
     if params[:query]
-      @maintenance_requests = MaintenanceRequest.search(params[:query]), page: params[:page], per_page: 20
+      @maintenance_requests = MaintenanceRequest.search(params[:query], page: params[:page], per_page: 20)
     
     else
       @maintenance_requests = []
