1937e93a8d96fc7dc350645620b8f97c04fc38d4
ronval <ronald.m.valencia@gmail.com>
2017-03-07 23:36:32 +1100

    fixed many bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,3 +87,15 @@ Rails.application.configure do
   #Variant setting for react-rails
   config.react.variant = :production
 end
+
+
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'maintenance-app-staging.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
