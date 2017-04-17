b0410d637ab42bc4581a6bfb67a53b7c21713ab8
whiteBear0510 <angelo.swt.n1@gmx.ch>
2017-04-18 05:56:41 +1000

    form-validation-maintenance-request-new

--- app/assets/javascripts/components/maintenance_requests/_access_contact_fields.js.jsx
+++ app/assets/javascripts/components/maintenance_requests/_access_contact_fields.js.jsx
@@ -1,4 +1,3 @@
-
 var AccessContactField = React.createClass({
     getInitialState : function() {
         return {
@@ -18,6 +17,32 @@ var AccessContactField = React.createClass({
     removeField() {
         this.setState({remove: true});
     },
+    
+    validateEmail: function(inputText, e, emailid) {
+      var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      errorMessage = '';
+      if(!inputText.match(mailformat)) {
+        errorMessage = 'Invalid Email Address!';
+        document.getElementById(emailid).textContent = errorMessage;
+				e.target.classList.add("border_on_error");
+      } else {
+        document.getElementById(emailid).textContent = errorMessage;
+				e.target.classList.remove("border_on_error");
+      }
+    },
+
+    validatePhoneNumber:function(inputText, e, mobileid) {
+      var numbers = /^[0-9]+$/;
+      if(inputText.match(numbers)) {
+        errorMessage = '';
+        document.getElementById(mobileid).textContent = errorMessage;
+				e.target.classList.remove("border_on_error");
+      } else {
+        errorMessage = 'Invalid Mobile Number!';
+        document.getElementById(mobileid).textContent = errorMessage;
+				e.target.classList.add("border_on_error");
+      }
+    },
 
     render : function() {
       var accessContact = this.props.content;
@@ -25,6 +50,9 @@ var AccessContactField = React.createClass({
       if (accessContact) {
           x = accessContact.id;
       }
+			var $fullnameid = 'errorboxfullname'+x;
+			var $emailid='errorboxemail'+x;
+			var $mobileid='errorboxmobile'+x;
       return (
         <div className="accesscontactfield" style={{display: this.state.remove ? 'none' : 'block' }}>
           <fieldset>
@@ -40,17 +68,78 @@ var AccessContactField = React.createClass({
   		  	  <p> Name </p>
   		  	  <input type="text" placeholder="Full name"
   		  	  		 name={this.generateAtt("name", x, "name")}
-  		  	  		   id={this.generateAtt("id", x, "name")} />
+  		  	  		   id={this.generateAtt("id", x, "name")}
+							 onBlur={(e) => {
+								let valid = true;
+								let errorMessage = '';
+								if (!e.target.value.length) {
+										valid = false;
+										errorMessage = 'This field is required';
+										document.getElementById($fullnameid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								else if(e.target.value.length < 4) {
+										valid = false;
+										errorMessage = 'Full Name must be more than 3 letters';
+										document.getElementById($fullnameid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								else if(e.target.value.length >= 4) {
+										valid = false;
+										errorMessage = '';
+										document.getElementById($fullnameid).textContent = errorMessage;
+										e.target.classList.remove("border_on_error");
+									}
+								}} />
+					<p id={$fullnameid} className="error"></p>
 
-  		  	  <p> Email </p>
-   	          <input type="email" placeholder="E-mail"
-   	           		 name={this.generateAtt("name", x, "email")}
-   	           		   id={this.generateAtt("id", x, "email")} />
-   	           		   
- 	      	  <p> Mobile </p>
-	          <input type="tel" placeholder="Mobile"
-	          		 name={this.generateAtt("name", x, "mobile")}
-	          		   id={this.generateAtt("id", x, "mobile")} />
+  		  	<p> Email </p>
+					<input type="email" placeholder="E-mail"
+								name={this.generateAtt("name", x, "email")}
+									id={this.generateAtt("id", x, "email")} 
+							onBlur={(e) => {
+								let valid = true;
+								let errorMessage = '';
+								if (!e.target.value.length) {
+										valid = false;
+										errorMessage = 'This field is required';
+										document.getElementById($emailid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								else if(e.target.value.length < 4) {
+										valid = false;
+										errorMessage = 'Name must be more than 3 letters';
+										document.getElementById($emailid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								else if(e.target.value.length >= 4) {
+										this.validateEmail(e.target.value, e, $emailid);
+									}
+								}}  />
+					<p id={$emailid} className="error"></p>
+
+					<p> Mobile </p>
+					<input type="tel" placeholder="Mobile"
+								name={this.generateAtt("name", x, "mobile")}
+									id={this.generateAtt("id", x, "mobile")}
+							onBlur={(e) => {
+								let valid = true;
+								let errorMessage = '';
+								if (!e.target.value.length) {
+										errorMessage = 'Mobile Number is required';
+										document.getElementById($mobileid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								else if(e.target.value.length < 8) {
+										errorMessage = 'Mobile Number must be more than 8 digits';
+										document.getElementById($mobileid).textContent = errorMessage;
+										e.target.classList.add("border_on_error");
+									}
+								if(e.target.value.length >= 8) {
+										this.validatePhoneNumber(e.target.value, e, $mobileid);
+									}
+								}} maxLength="10" />
+						<p id={$mobileid} className="error"></p>
 
             <input type="hidden" value={this.state.remove}
                 name={this.generateAtt("name", x, "_destroy")}
