9423143c02d1e8e189ebb56b4ac68f195b514b11
ronval <ronald.m.valencia@gmail.com>
2017-04-22 00:24:56 +1000

    added landlord show and index

--- app/controllers/landlord_maintenance_requests_controller.rb
+++ app/controllers/landlord_maintenance_requests_controller.rb
@@ -9,7 +9,59 @@ class LandlordMaintenanceRequestsController < ApplicationController
   end
 
   def show
+    @maintenance_request = MaintenanceRequest.find_by(id:params[:id])
+    # @tenants = @maintenance_request.tenants
+    @quotes = @maintenance_request.quotes.where(:delivery_status=>true)
+    @quote = @quotes.where(:status=>"Approved").first if !nil
+    @pdf_files = @maintenance_request.delivered_uploaded_invoices
+
+    if @quote
+      @quote_id = @quote.id
+    else
+      @quote_id = ''
+    end 
+
+    @message = Message.new
     
+    @tradie = Trady.new
+     
+    if @maintenance_request.maintenance_request_image != nil
+      @gallery = @maintenance_request.maintenance_request_image.images
+    end 
+
+    if  @maintenance_request.property.landlord != nil
+      @landlord = Landlord.find_by(id:@maintenance_request.property.landlord.id)
+    end 
+    
+    if @maintenance_request.agency_admin != nil
+      if @maintenance_request.agency_admin.agency.tradies !=nil
+        @all_tradies = @maintenance_request.agency_admin.agency.tradies.where(:skill=>@maintenance_request.service_type) 
+      else 
+        @all_tradies= []
+      end 
+    end 
+
+    if @maintenance_request.agent != nil
+      if @maintenance_request.agent.agency.tradies !=nil 
+        @all_tradies = @maintenance_request.agent.agency.tradies.where(:skill=>@maintenance_request.service_type) 
+      else 
+        @all_tradies= []
+      end
+    end 
+    
+
+    if @maintenance_request.conversations.where(:conversation_type=>"Tenant").present?
+      @tenants_conversation = @maintenance_request.conversations.where(:conversation_type=>"Tenant").first.messages
+    end 
+
+    if @maintenance_request.conversations.where(:conversation_type=>"Landlord").present?
+      @landlords_conversation = @maintenance_request.conversations.where(:conversation_type=>"Landlord").first.messages
+    end 
+
+    respond_to do |format|
+      format.json { render :json=>{:gallery=>@gallery.as_json, :quotes=> @quotes, :landlord=> @landlord, :all_tradies=> @all_tradies, :tenants_conversation=> @tenants_conversation,:landlords_conversation=> @landlords_conversation}}
+      format.html{render :show}
+    end 
   end
 
 end 
