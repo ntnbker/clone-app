e4b669d9c3bc1cdc3d1de29215d50942f70b4870
ronval <ronald.m.valencia@gmail.com>
2017-06-23 15:16:58 +1000

    code

--- app/controllers/maintenance_requests_controller.rb
+++ app/controllers/maintenance_requests_controller.rb
@@ -2,7 +2,7 @@ class MaintenanceRequestsController < ApplicationController
   
   # before_action(only: [:show]) { email_auto_login(params[:user_id]) }
   # before_action(only: [:show]) { maintenance_request_stakeholders(params[:id]) }
-  before_action :require_login,(only:[:update])
+  before_action :require_login, only:[:update]
   before_action :set_user, only:[:new,:create]
   
   before_action :customer_input_session, only:[:create,:new]
