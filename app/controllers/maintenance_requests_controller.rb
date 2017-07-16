e3e50f997871861be2e41034730ee249999eb432
ronval <ronald.m.valencia@gmail.com>
2017-07-16 22:25:01 +1000

    code

--- app/controllers/maintenance_requests_controller.rb
+++ app/controllers/maintenance_requests_controller.rb
@@ -93,7 +93,7 @@ class MaintenanceRequestsController < ApplicationController
       ############################    
       if @user && existing_role == false
         role = Role.new(user_id:@user.id)
-        @tenant = Tenant.create(user_id:@user.id,full_name:params[:maintenance_request][:name],email:params[:maintenance_request][:email],mobile:params[:maintenance_request][:mobile])
+        @tenant = Tenant.create(user_id:@user.id,name:params[:maintenance_request][:name],email:params[:maintenance_request][:email],mobile:params[:maintenance_request][:mobile])
         @tenant.roles << role
         role.save
         
@@ -277,7 +277,7 @@ class MaintenanceRequestsController < ApplicationController
       the_url = agency_admin_maintenance_request_url(@maintenance_request)
      
       EmailWorker.perform_async(@maintenance_request.id)
-      AgencyAdminOrAgentNewMaintenanceRequestNotificationTextingWorker.perform_async(@maintenance_request.id,the_url)
+      #AgencyAdminOrAgentNewMaintenanceRequestNotificationTextingWorker.perform_async(@maintenance_request.id,the_url)
 
       MaintenanceRequest.last.reindex
       if current_user == nil
