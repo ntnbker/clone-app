c2ca35479feb7f4a78a5be038b3529aa7be684d4
ronval <ronald.m.valencia@gmail.com>
2017-07-20 21:55:49 -0400

    code

--- app/mailers/tenant_mailer.rb
+++ app/mailers/tenant_mailer.rb
@@ -1,5 +1,5 @@
 class TenantMailer < ActionMailer::Base
-
+  default from: 'info@maintenanceapp.com.au'
   def send_tenant_initial_appointment_request(maintenance_request,appointment, tenant, trady)
 
     @appointment = appointment
@@ -13,24 +13,26 @@ class TenantMailer < ActionMailer::Base
 
   def appointment_accepted_email(maintenance_request_object,appointment_object, trady_object, tenant_object)
     @maintenance_request = maintenance_request_object
+    @property = @maintenance_request
     @appointment = appointment_object
     @trady = trady_object
     @tenant = tenant_object
     track user: @tenant.user
     track extra: {maintenance_request_id:@maintenance_request.id}
-    mail(from:"ron@email.com",to:@tenant.email, subject:"Hi your appointment time was accepted")
+    mail(to:@tenant.email, subject:"Appointment confirmed by #{@trady.trady_company.company_name.capitalize} - #{@property.property_address}")
   end
 
   
   def alternative_appointment_picked_email(maintenance_request_object,appointment_object, trady_object, tenant_object)
     @maintenance_request = maintenance_request_object
     @appointment = appointment_object
+    @property = @maintenance_request.property
     @trady = trady_object
     @tenant = tenant_object
     @user = @tenant.user
     track user: @trady.user
     track extra: {maintenance_request_id:@maintenance_request.id}
-    mail(from:"ron@email.com",to:@tenant.email, subject:"New Appointment Time Requested")
+    mail(to:@tenant.email, subject:"New appointment request by #{@trady.trady_company.company_name.capitalize} - #{@property.property_address.capitalize}")
   end
 
   def send_tenant_initial_landlord_appointment_request(maintenance_request,appointment, tenant, landlord)
@@ -46,23 +48,25 @@ class TenantMailer < ActionMailer::Base
 
   def alternative_landlord_appointment_picked_email(maintenance_request_object,appointment_object, landlord_object, tenant_object)
     @maintenance_request = maintenance_request_object
+    @property = @maintenance_request.property
     @appointment = appointment_object
     @landlord = landlord_object
     @tenant = tenant_object
     @user = @tenant.user
     track user: @tenant.user
     track extra: {maintenance_request_id:@maintenance_request.id}
-    mail(from:"ron@email.com",to:@tenant.email, subject:"New Appointment Time Requested")
+    mail(to:@tenant.email, subject:" New appointment request by landlord, #{@landlord.name.capitalize} - @property.property_address")
   end
 
   def appointment_accepted_by_landlord_email(maintenance_request_object,appointment_object, landlord_object, tenant_object)
     @maintenance_request = maintenance_request_object
+    @property = @maintenance_request.property
     @appointment = appointment_object
     @landlord = landlord_object
     @tenant = tenant_object
     track user: @tenant.user
     track extra: {maintenance_request_id:@maintenance_request.id}
-    mail(from:"ron@email.com",to:@tenant.email, subject:"Hi your appointment time was accepted")
+    mail(to:@tenant.email, subject:"Appointment confirmed by Landlord, #{@landlord.name.capitalize} - #{@property.property_address}")
   end
 
   def trady_cancelled_appointment_email(tenant_object)
