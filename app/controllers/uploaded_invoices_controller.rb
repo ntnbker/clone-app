ca41db5b9f3fca93af6c1b8c9de03e84ee14f675
ronval <ronald.m.valencia@gmail.com>
2017-08-03 03:36:38 -0400

    added url to uploaded invoice table

--- app/controllers/uploaded_invoices_controller.rb
+++ app/controllers/uploaded_invoices_controller.rb
@@ -20,9 +20,9 @@ class UploadedInvoicesController < ApplicationController
     invoice_type = params[:uploaded_invoice][:invoice_type]
     system_plan = params[:uploaded_invoice][:system_plan]
 
-    if @file.valid?
+    if @file.save
       flash[:success] = "Thank you for uploading your invoice(s)"
-      @file.save
+      @file.update_attribute(:url, @file.pdf_url)
       redirect_to uploaded_invoice_path(@file, maintenance_request_id:maintenance_request_id, trady_id:trady_id, quote_id:quote_id, invoice_type:invoice_type, system_plan:system_plan)
     else
       flash[:danger] = "Something went wrong."
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20170728025357) do
+
+ActiveRecord::Schema.define(version: 20170803072625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -446,6 +447,7 @@ ActiveRecord::Schema.define(version: 20170728025357) do
     t.boolean  "delivery_status"
     t.text     "invoice_data"
     t.text     "pdf_data"
+    t.text     "url"
   end
 
   create_table "uploaded_quotes", force: :cascade do |t|
