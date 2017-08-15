afa6cc0dcda458c25f48b0aa5883245314b44d9c
Unknown <ngothanhtruong92@gmail.com>
2017-08-15 13:13:57 +0700

    update

--- app/assets/javascripts/components/maintenance_requests/add_photo.js.jsx
+++ app/assets/javascripts/components/maintenance_requests/add_photo.js.jsx
@@ -294,9 +294,9 @@ var ModalAddPhoto = React.createClass({
     var FD = new FormData();
     this.state.dataImages.map((image, index) => {
       var idx = index + 1;
-      FD.append('image['+ idx + ']', JSON.stringify(image));
+      FD.append('picture[image]['+ idx + ']', JSON.stringify(image));
     });
-    FD.append('maintenance_request_id', this.props.maintenance_request.id);
+    FD.append('picture[maintenance_request_id]', this.props.maintenance_request.id);
 
     var props = this.props;
     $.ajax({
