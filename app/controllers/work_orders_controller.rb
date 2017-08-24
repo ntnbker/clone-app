9f8484e6082b1bdfcb05942465375f464ccaeac9
ronval <ronald.m.valencia@gmail.com>
2017-08-24 00:11:33 -0400

    added cancel work order

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -2,6 +2,10 @@ class WorkOrdersContoller < ApplicationController
   def cancel_work_order
     maintenance_request = MaintenanceRequest.find_by(id:params[:maintenance_request_id])
     maintenance_request.update_attribute(:trady_id, nil)
+    log = Log.create(maintenance_request_id:maintenance_request.id, action:"Work order cancelled.")
+    respond_to do |format|
+      format.json {render :json=>{log:log}}
+    end 
   end
 
 end 
