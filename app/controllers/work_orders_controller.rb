fd0f170c9bbff2f91c80c486f9cc831d43cd1af7
ronval <ronald.m.valencia@gmail.com>
2017-08-24 00:26:29 -0400

    fixed typo

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -1,4 +1,4 @@
-class WorkOrdersContoller < ApplicationController
+class WorkOrdersController < ApplicationController
   def cancel_work_order
     maintenance_request = MaintenanceRequest.find_by(id:params[:maintenance_request_id])
     maintenance_request.update_attribute(:trady_id, nil)
