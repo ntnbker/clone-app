2f7e5fbde97f95a6eeb724974e149355f0bb3d90
ronval <ronald.m.valencia@gmail.com>
2017-09-01 04:28:24 -0400

    code

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -3,8 +3,12 @@ class WorkOrdersController < ApplicationController
     maintenance_request = MaintenanceRequest.find_by(id:params[:maintenance_request_id])
     maintenance_request.update_attribute(:trady_id, nil)
     log = Log.create(maintenance_request_id:maintenance_request.id, action:"Work order cancelled.")
-    respond_to do |format|
-      format.json {render :json=>{log:log}}
+    if current_user.logged_in_as("AgencyAdmin")
+      
+      redirect_to agency_admin_maintenance_request_path(maintenance_request)
+    elsif current_user.logged_in_as("Agent")
+      
+      redirect_to agent_maintenance_request_path(maintenance_request)
     end 
   end
 
