6afe88c7648522a2ef6c31d27dee69b4d7f8b5e6
ronval <ronald.m.valencia@gmail.com>
2017-10-18 13:30:20 +1100

    added controller code and models for agents

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -4,6 +4,7 @@ class Agent < ApplicationRecord
   has_many :action_statuses
   belongs_to :agency
   belongs_to :user
+  has_one :agent_profile_image
   
   validates_presence_of :name,:email, :mobile_phone, :last_name, :license_number
   validates_uniqueness_of :email
