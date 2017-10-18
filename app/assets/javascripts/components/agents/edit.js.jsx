cb9c10b18b89c68ddc34c72cccac2c7f05ce4893
jeff <ntnbker@gmail.com>
2017-10-18 11:27:32 +0700

    add edit agent form

--- app/assets/javascripts/components/agents/edit.js.jsx
+++ app/assets/javascripts/components/agents/edit.js.jsx
@@ -1,10 +1,9 @@
-var AgencyEdit = React.createClass({
+var AgentEdit = React.createClass({
   getInitialState: function () {
-    const agency = this.props.agency || {};
+    const agent = this.props.agent || {};
     const image_url = this.props.image_url;
 
     return {
-      license_type: agency.license_type,
       errors: {},
       gallery: image_url && { image_url } || null,
     };
@@ -21,7 +20,7 @@ var AgencyEdit = React.createClass({
       FD.append('picture[image]', JSON.stringify(image));
     });
 
-    FD.append('picture[agency_id]', this.props.agency.id);
+    FD.append('picture[agent_id]', this.props.agent.id);
 
     const self = this;
     $.ajax({
@@ -61,7 +60,7 @@ var AgencyEdit = React.createClass({
 
     const getValidValue = obj => obj && obj.value;
 
-    var agency = {
+    var agent = {
       company_name              : getValidValue(company_name),
       business_name             : getValidValue(business_name),
       abn                       : getValidValue(abn),
@@ -73,12 +72,12 @@ var AgencyEdit = React.createClass({
       license_type              : this.state.license_type,
     }
 
-    var params = { agency };
+    var params = { agent };
 
     const self = this;
     $.ajax({
       type: 'PUT',
-      url: `/agencies/${(this.props.agency || {}).id}`,
+      url: `/agencies/${(this.props.agent || {}).id}`,
       beforeSend: function(xhr) {
         xhr.setRequestHeader('X-CSRF-Token', self.props.authenticity_token);
       },
@@ -115,8 +114,8 @@ var AgencyEdit = React.createClass({
 
   renderTextField: function(field, textHolder) {
     const { errors }      = this.state;
-    const { agency = {} } = this.props;
-    const value           = agency[field];
+    const { agent = {} } = this.props;
+    const value           = agent[field];
 
     return (
       <div className="form-group">
@@ -139,58 +138,35 @@ var AgencyEdit = React.createClass({
 
   render: function() {
     let isInvoice          = this.props.system_plan === "Invoice";
-    let agency             = this.props.agency || {};
+    let agent              = this.props.agent || {};
     let {
       company_name, business_name, abn, address, mailing_address, phone, mobile_phone, corporation_license_number
-    } = agency;
+    } = agent;
 
     let { errors = {}, license_type, gallery } = this.state;
     const renderTextField                      = this.renderTextField;
     const renderButtonFunc                     = this.renderButton;
 
     return (
-      <div className="edit_agency">
+      <div className="edit_agent">
         <div className="left">
-          {renderButtonFunc('Add New Agency', this.props.new_agent_path)}
-          {renderButtonFunc('Add New Admin Agency', this.props.new_agency_admin_path)}
+          { gallery &&
+              <img id="avatar" src={gallery.image_url} alt="Avatar Image"/>
+          }
+          <ModalImageUpload
+            uploadImage={this.uploadImage}
+            gallery={gallery && [gallery] || []}
+            text="Add/Change Photo"
+            className="btn btn-default btn-back m-r-lg"
+          />
+          {renderButtonFunc('Reset Password', this.props.change_password_path)}
         </div>
-        <form role="form" className="form-horizontal right" id="edit_agency_admin" onSubmit={this.onSubmit} >
-          {renderTextField('company_name', 'Company Name')}
-          {renderTextField('business_name', 'Business Name')}
-          {renderTextField('abn', 'ABN')}
-          {renderTextField('address', 'Address')}
-          {renderTextField('mailing_address', 'Mailing Address')}
+        <form role="form" className="form-horizontal right" id="edit_agent" onSubmit={this.onSubmit} >
+          {renderTextField('name', 'Name')}
+          {renderTextField('last_name', 'Last Name')}
           {renderTextField('phone', 'Phone')}
           {renderTextField('mobile_phone', 'Mobile Phone')}
-          <div className="license-type">
-            <p> License Type </p>
-            <label className="one-half column">
-              <input
-                type="radio"
-                value="Individual License"
-                checked={license_type === "Individual License"}
-                ref={e => this.license_type_individual_license = e}
-                name="license_type"
-                id="license_type_individual_license"
-                onChange={this.changeLicenseType}
-              />
-              Individual License
-            </label>
-            <label className="one-half column">
-              <input
-                type="radio"
-                value="Corporate License"
-                checked={license_type === "Corporate License"}
-                ref={e => this.license_type_corporate_license = e}
-                name="license_type"
-                id="license_type_corporate_license"
-                onChange={this.changeLicenseType}
-              />
-              Corporate License
-            </label>
-            {this.renderError(errors['license_type'])}
-          </div>
-          {renderTextField('corporation_license_number', 'Corporation License Number')}
+          {renderTextField('license_number', 'License Number')}
           <div className="text-center">
             <button type="submit" className="button-primary green option-button">
               Update Your Profile
