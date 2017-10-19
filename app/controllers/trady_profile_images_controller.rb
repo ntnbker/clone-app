a1087371f3a82185daf8b127b293aeafd0c6cddf
ronval <ronald.m.valencia@gmail.com>
2017-10-19 22:35:05 +1100

    fixed a typo in the class name

---
+++ app/controllers/trady_profile_images_controller.rb
@@ -0,0 +1,45 @@
+class TradyProfileImagesController < ApplicationController
+  def create
+    
+    
+    @image =  TradyProfileImage.new(image_params)
+
+    if @image.save
+      profile_image = @image.as_json(methods: :image_url)
+      
+      respond_to do |format|
+        format.json {render :json=>{:profile_image=>profile_image}}
+      end 
+    else
+      respond_to do |format|
+        format.json {render :json=>{error=>image.errors}}
+      end
+    end 
+    
+  end
+
+  def update
+
+    @image =  TradyProfileImage.find_by(id:params[:picture][:trady_profile_image_id])
+
+    if @image.update(image_params)
+      profile_image = @image.as_json(methods: :image_url)
+      
+      respond_to do |format|
+        format.json {render :json=>{:profile_image=>profile_image}}
+      end 
+    else
+      respond_to do |format|
+        format.json {render :json=>{error=>image.errors}}
+      end
+    end 
+
+  end
+
+  private
+
+  def image_params
+    params.require(:picture).permit(:image, :trady_id, :trady_profile_image_id)
+  end
+
+end 
