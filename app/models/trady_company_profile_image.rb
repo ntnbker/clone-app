ed6395fe26e2228b2b3c9fe929fe35a365a4b96a
ronval <ronald.m.valencia@gmail.com>
2017-10-19 14:43:01 +1100

    code

--- app/models/trady_company_profile_image.rb
+++ app/models/trady_company_profile_image.rb
@@ -1,4 +1,6 @@
 class TradyCompanyProfileImage < ApplicationRecord
    include TradyCompanyProfileImageUploader::Attachment.new(:image) # adds an `image` virtual attribute
   belongs_to :trady_company
+
+  attr_accessor :trady_company_profile_image_id
 end 
