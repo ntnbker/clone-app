7b9105bf6b43c4d86a6c6fcb79900d7381f081e5
ronval <ronald.m.valencia@gmail.com>
2017-10-26 19:34:25 +1100

    removed old code

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -10,7 +10,7 @@ class Agency < ApplicationRecord
   validates :company_name, uniqueness: true
   validates :business_name, uniqueness: true
   validates :abn, uniqueness: true
-  validates :mobile_phone, uniqueness: true
+  
   validates :license_number, uniqueness:true 
   
   
@@ -19,6 +19,7 @@ class Agency < ApplicationRecord
   validates_presence_of :abn
   validates_presence_of :address
   validates_presence_of :mobile_phone
+  validates_presence_of :phone
   validates_presence_of :license_type
   validates_presence_of :license_number
   
@@ -29,7 +30,7 @@ class Agency < ApplicationRecord
 
   
   validates :mobile_phone, :numericality => true, :length => {:minimum=>10, :maximum => 10 }
-  
+  validates :phone, :numericality => true, :length => {:minimum=>10, :maximum => 10 }
   validates :abn, :numericality => true, :length => {:minimum=>11, :maximum => 11 }
   
 
