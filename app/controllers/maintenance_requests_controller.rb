6dc7a30f634937e61cb4d47e03224efa32aca636
ronval <ronald.m.valencia@gmail.com>
2017-11-15 17:12:39 +1100

    Added preapproved note

--- app/controllers/maintenance_requests_controller.rb
+++ app/controllers/maintenance_requests_controller.rb
@@ -373,7 +373,18 @@ class MaintenanceRequestsController < ApplicationController
 
   def preapproved
     maintenance_request = MaintenanceRequest.find_by(id:params[:maintenance_request_id])
-    maintenance_request.update_attribute(:preapproved_amount, params[:preapproved_note])
+
+    if params[:preapproved_note]
+      maintenance_request.update_attribute(:preapproved_amount, params[:preapproved_note])
+      respond_to do |format|
+        format.json {render :json=>{:preapproved_note=>params[:preapproved_note]}}
+      end
+    elsif params[:preapproved_note].empty?
+      respond_to do |format|
+        format.json {render :json=>{:errors=>{"Note must not be blank thank you."}}}
+      end 
+    end 
+      
   end
 
 
