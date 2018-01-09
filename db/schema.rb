55465709eede0a84c4c4408eff95940845f5243b
ronval <ronald.m.valencia@gmail.com>
2018-01-09 16:48:34 +1100

    added validations to backend for landline and profession license

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220030721) do
+ActiveRecord::Schema.define(version: 20180109053142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -461,12 +461,14 @@ ActiveRecord::Schema.define(version: 20171220030721) do
     t.string   "mailing_address"
     t.string   "mobile_number"
     t.string   "email"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.string   "account_name"
     t.string   "bsb_number"
     t.string   "bank_account_number"
     t.integer  "trady_id"
+    t.string   "profession_license_number"
+    t.string   "landline"
   end
 
   create_table "trady_company_profile_images", force: :cascade do |t|
