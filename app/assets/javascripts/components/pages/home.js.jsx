252487efbfc1ac062609fdd670f8ed11640e2d2d
jeff <ntnbker@gmail.com>
2018-01-18 12:12:48 +0700

    build action for tenants

--- app/assets/javascripts/components/pages/home.js.jsx
+++ app/assets/javascripts/components/pages/home.js.jsx
@@ -2,6 +2,7 @@ var HomeComponent = React.createClass({
   getInitialState: function() {
     return {
       active: 'tenant',
+      step: 'newMR',
     };
   },
 
@@ -16,23 +17,23 @@ var HomeComponent = React.createClass({
       <div className="row how-it-works">
         <div className="three columns">
           <img src="/assets/key1.png" alt=""/>
-          <h4>Maintenance requested</h4>
-          <p>Fill in the Maintenance form and submit the request</p>
+          <h4>Maintenance Requested</h4>
+          <p>Receive Maintenance Requests in a centralized app</p>
         </div>
         <div className="three columns">
           <img src="/assets/key2.png" alt=""/>
-          <h4>Maintenance actioned</h4>
-          <p>Sit back, and let us sort out the maintenance for you!</p>
+          <h4>Easily action property <br /> maintenance requests</h4>
+          <p>Quickly communicate with your agent on how maintenance should be handled</p>
         </div>
         <div className="three columns">
           <img src="/assets/key3.png" alt=""/>
-          <h4>Track Maintenance Status</h4>
-          <p>Get updates on your maintenance request is up or check online</p>
+          <h4>Receive multiple competative quotes</h4>
+          <p>Quickly request quotes from multiple tradies to receive competative prices</p>
         </div>
         <div className="three columns">
           <img src="/assets/key4.png" alt=""/>
-          <h4>Maintenance complete</h4>
-          <p>Enjoy a stress free process to maintain your home</p>
+          <h4>Maintenance Complete</h4>
+          <p>Enjoy a stress free process to maintain your property</p>
         </div>
       </div>
     )
@@ -43,23 +44,23 @@ var HomeComponent = React.createClass({
       <div className="row how-it-works">
         <div className="three columns">
           <img src="/assets/key1.png" alt=""/>
-          <h4>Maintenance requested</h4>
-          <p>Fill in the Maintenance form and submit the request</p>
+          <h4>Quickly organize maintenance requests</h4>
+          <p>Instantly deal with maintenance requests submitted by tenants</p>
         </div>
         <div className="three columns">
           <img src="/assets/key2.png" alt=""/>
-          <h4>Maintenance actioned</h4>
-          <p>Sit back, and let us sort out the maintenance for you!</p>
+          <h4>Time is money easily <br /> increase your productivity</h4>
+          <p>With a just few clicks you can make sure the job is completed</p>
         </div>
         <div className="three columns">
           <img src="/assets/key3.png" alt=""/>
-          <h4>Track Maintenance Status</h4>
-          <p>Get updates on your maintenance request is up or check online</p>
+          <h4>Track maintenance status</h4>
+          <p>Automated reminders unsure jobs are progressing</p>
         </div>
         <div className="three columns">
           <img src="/assets/key4.png" alt=""/>
-          <h4>Maintenance complete</h4>
-          <p>Enjoy a stress free process to maintain your home</p>
+          <h4>Increase revenue</h4>
+          <p>Increase revenue through our rebate program and increased productivity</p>
         </div>
       </div>
     )
@@ -70,23 +71,23 @@ var HomeComponent = React.createClass({
       <div className="row how-it-works">
         <div className="three columns">
           <img src="/assets/key1.png" alt=""/>
-          <h4>Maintenance requested</h4>
-          <p>Fill in the Maintenance form and submit the request</p>
+          <h4>Receive High Quality <br /> Job Leads</h4>
+          <p>Receive maintenance work from property and strata managers</p>
         </div>
         <div className="three columns">
           <img src="/assets/key2.png" alt=""/>
-          <h4>Maintenance actioned</h4>
-          <p>Sit back, and let us sort out the maintenance for you!</p>
+          <h4>Easily track job information <br /> and schedule jobs</h4>
+          <p>Easily organize job communications and appointments</p>
         </div>
         <div className="three columns">
           <img src="/assets/key3.png" alt=""/>
-          <h4>Track Maintenance Status</h4>
-          <p>Get updates on your maintenance request is up or check online</p>
+          <h4>Easily create invoices <br /> or upload your own</h4>
+          <p>Easily submit invoices. With Automated invoice reminders. We help make sure you get paid</p>
         </div>
         <div className="three columns">
           <img src="/assets/key4.png" alt=""/>
-          <h4>Maintenance complete</h4>
-          <p>Enjoy a stress free process to maintain your home</p>
+          <h4>Increase your revenue</h4>
+          <p>Join our network to grow your business</p>
         </div>
       </div>
     )
@@ -130,11 +131,11 @@ var HomeComponent = React.createClass({
 
   },
 
-  homeActionForTenant() {
+  homeActionForTenantLogin() {
     const { signed = false } = this.props;
 
     return (
-      <div className="action-content tenant-content">
+      <div className="tenant-content">
         <div className="login-info">
           <div className="login-input email-input">
             <span className="email">
@@ -149,12 +150,90 @@ var HomeComponent = React.createClass({
             <input type="password" />
           </div>
         </div>
-        <div className="button-group tenant-buttons">
-          <button type="button" className="btn btn-back">Back</button>
-          <button type="button" className="btn btn-login">Login</button>
+        <div className="button-group login-button">
+          <button
+            type="button"
+            className="btn btn-back"
+            onClick={() => this.setState({step: 'home'})}
+          >
+            Back
+          </button>
+          <button
+            type="button"
+            className="btn btn-login"
+            onClick={() => this.setState({step: 'home'})}
+          >
+            Login
+          </button>
         </div>
         <div className="forget-password">
           <a href="#" className="link-forget-password">Forgot Password</a>
+        </div>
+      </div>
+    )
+  },
+
+  homeActionForTenant() {
+    const { signed = false } = this.props;
+
+    return (
+      <div className="button-group tenant-home-button">
+        <button type="button text-center" className="btn">
+          Check My Maintenance Requests
+        </button>
+        <p className="text-center">Or</p>
+        <button
+          type="button text-center"
+          className="btn"
+          onClick={() => this.setState({step: 'newMR'})}
+        >
+          Submit New Maintenance Request
+        </button>
+      </div>
+    )
+  },
+
+  homeActionForSubmitNewMR() {
+    const { signed = false, services } = this.props;
+
+    return (
+      <div className="submit-new-mr">
+        <div className="type-service">
+          <select className="type-service" id="select-type-service">
+            <option value={null}>What type of service do you require?</option>
+            {
+              services.map(({service, id}) => {
+                return (
+                  <option value={service} key={id}>{service}</option>
+                )
+              })
+            }
+          </select>
+          <span className="dropdown-arrow fa fa-caret-down"></span>
+        </div>
+        <div className="input-address">
+          <textarea
+            type="text"
+            id="pac-input"
+            placeholder="Where do need the service done? Please tell us the address."
+            onChange={getAddressOfGoogleMap}
+          />
+        </div>
+        <div className="button-group login-button new-mr-button">
+          <button
+            type="button"
+            className="btn btn-back"
+            onClick={() => this.setState({step: 'home'})}
+          >
+            Back
+          </button>
+          <button
+            type="button"
+            className="btn btn-summit"
+            onClick={() => this.setState({step: 'home'})}
+          >
+            Submit
+          </button>
         </div>
       </div>
     )
@@ -193,10 +272,35 @@ var HomeComponent = React.createClass({
   },
 
   homeActionContent() {
+    const { active, step } = this.state;
+    let render = null;
+
+    switch (active) {
+      case 'tenant':
+        if (step === 'login') {
+          render = this.homeActionForTenantLogin;
+        }
+        if (step === 'home') {
+          render = this.homeActionForTenant;
+        }
+        if (step === 'newMR') {
+          render = this.homeActionForSubmitNewMR;
+        }
+        break;
+      case 'trady':
+        render = this.homeActionForTenantLogin;
+        break;
+      case 'landlord':
+        render = this.homeActionForTenantLogin;
+        break;
+      case 'agent':
+        render = this.homeActionForTenantLogin;
+        break;
+    }
     return (
       <div className="home-action-content">
         <div className="content-frame"></div>
-        {this.homeActionForTenant()}
+        <div className="action-content">{render && render()}</div>
       </div>
     )
   },
