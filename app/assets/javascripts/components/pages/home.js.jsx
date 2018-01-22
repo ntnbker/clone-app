2b6a42fa83741a042956204e7fdda20cd8b96f01
jeff <ntnbker@gmail.com>
2018-01-22 09:55:03 +0700

    change title when signed in

--- app/assets/javascripts/components/pages/home.js.jsx
+++ app/assets/javascripts/components/pages/home.js.jsx
@@ -461,7 +461,7 @@ var HomeComponent = React.createClass({
           }
           onClick={() => this.chooseUser('Tenant')}
         >
-          For Tenants
+          {!signed && For }Tenants
         </button>
         <button
           className={
@@ -470,7 +470,7 @@ var HomeComponent = React.createClass({
           }
           onClick={() => this.chooseUser('Trady')}
         >
-          For Tradies
+          {!signed && For }Tradies
         </button>
         <button
           className={
@@ -479,7 +479,7 @@ var HomeComponent = React.createClass({
           }
           onClick={() => this.chooseUser('Landlord')}
         >
-          For Landlords
+          {!signed && For }Landlords
         </button>
         <button
           className={
@@ -488,7 +488,7 @@ var HomeComponent = React.createClass({
           }
           onClick={() => this.chooseUser('Agent')}
         >
-          For Agents
+          {!signed && For }Agents
         </button>
       </div>
     )
