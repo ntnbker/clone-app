c6c273c89f5b8ad8a0c0d0ae3f879d458c05be92
jeff <ntnbker@gmail.com>
2018-02-01 14:02:57 +0700

    change upload quote request text

--- app/assets/javascripts/components/quotes/index.js.jsx
+++ app/assets/javascripts/components/quotes/index.js.jsx
@@ -628,7 +628,7 @@ var QuoteRequests = React.createClass({
 													className="btn btn-default"
 													{...self}
 													chooseImageText="Choose Image or PDF to upload"
-													text="Upload Quote"
+													text="Upload Quote PDF/Image"
 													onClick={() => self.chooseQuoteRequest(quote_request)}
 												/>
 											: ''
