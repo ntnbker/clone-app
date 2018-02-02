117f07332adc74276efca71572014255a26a40b8
ronval <ronald.m.valencia@gmail.com>
2018-02-02 18:45:11 +1100

    added validations for the quote_items

--- app/models/quote_item.rb
+++ app/models/quote_item.rb
@@ -2,4 +2,6 @@ class QuoteItem < ApplicationRecord
   belongs_to :quote, inverse_of: :quote_items
   validates_presence_of :item_description, :amount
   validates :amount, :numericality => true
+  validates :min_price, :numericality => true
+  validates :max_price, :numericality => true
 end 
