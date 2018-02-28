5c0e1a4bb0dad7692b135cf7abf14ec9dd7e137e
ronval <ronald.m.valencia@gmail.com>
2018-02-28 16:34:21 +1100

    added redirect for the terms and conditions back button

--- app/controllers/tradie_term_agreements_controller.rb
+++ app/controllers/tradie_term_agreements_controller.rb
@@ -1,5 +1,9 @@
 class TradieTermAgreements < ApplicationController
   def new
+    @maintenance_request_id = params[:maintenance_request_id]
+    @trady_company_id = params[:trady_company_id]
+    @trady_id = params[:trady_id]
+
     
   end
 
