592d84964b7c1329146da8c9db9c2790451dfa87
ronval <ronald.m.valencia@gmail.com>
2018-03-06 14:17:34 +1100

    added variable for customer in the uplaod invoice show page

--- app/controllers/agency_payment_registrations_controller.rb
+++ app/controllers/agency_payment_registrations_controller.rb
@@ -5,7 +5,34 @@ class AgencyPaymentRegistrationsController < ApplicationController
   end
 
   def create
-    
+    trady = Trady.find_by(id:params[:trady_id])
+    agency = Agency.find_by(id:params[:agency_id])
+
+
+    if agency.customer_profile && agency.customer_profile.customer_id == nil
+      
+
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+      agency.customer_profile.update_attribute(:customer_id, customer.id)
+
+    elsif trady.customer_profile == nil
+
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+
+      CustomerProfile.create(customer_id:customer.id, trady_id:trady.id)
+      
+    end 
+
+    respond_to do |format|
+          format.json{render :json=>{message:"Thank you for signing up with MaintenanceApp"}}
+          
+      end
   end
 
 end 
