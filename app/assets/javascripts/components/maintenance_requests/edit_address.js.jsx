ba03138faca6b969d20c12a33054d3d6d1f4d696
daniel <danielhoang79@gmail.com>
2018-03-16 15:24:15 +0700

    update css edit address form

--- app/assets/javascripts/components/maintenance_requests/edit_address.js.jsx
+++ app/assets/javascripts/components/maintenance_requests/edit_address.js.jsx
@@ -1,31 +1,30 @@
 var ModalEditAddress = React.createClass({
   render: function() {
     return (
-      <div className="modal-custom fade">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
+      <div className='modal-custom fade'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
               <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
+                type='button'
+                className='close'
+                data-dismiss='modal'
+                aria-label='Close'
                 onClick={this.props.close}
               >
-                <span aria-hidden="true">&times;</span>
+                <span aria-hidden='true'>&times;</span>
               </button>
-              <h4 className="modal-title text-center">Edit Address</h4>
+              <h4 className='modal-title text-center'>Edit Address</h4>
             </div>
-            <div className="modal-body">
-              <form role="form" id="edit_address" action="/edit_address" acceptCharset="UTF-8" method="post">
+            <div className='modal-body'>
+              <form role='form' id='edit_address' action='/edit_address' acceptCharset='UTF-8' method='post'>
                 <input
                   id='pac-input'
-                  className='form-group'
                   type='text'
                   name='address'
-                  placeholder="Please tell us the address."
+                  placeholder='Please tell us the address.'
                   onChange={getAddressOfGoogleMap}/>
-                <button name="button" type="submit" className="button-primary green">Update</button>
+                <button name='button' type='submit' className='button-primary green'>Update</button>
               </form>
             </div>
           </div>
