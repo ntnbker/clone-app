856f08cbc2adfe15f0985b464d045351afb9276f
ronval <ronald.m.valencia@gmail.com>
2018-03-20 18:59:26 +1100

    added url redirect for t&c to trady reg

--- app/controllers/tradie_term_agreements_controller.rb
+++ app/controllers/tradie_term_agreements_controller.rb
@@ -12,15 +12,9 @@ class TradieTermAgreementsController < ApplicationController
     if  params[:terms_and_conditions] == "true" 
       trady = Trady.find_by(id:params[:trady_id])
       CustomerProfile.create(trady_id:trady.id, terms_and_conditions: true)
+      flash[:success] = "Thank you for joining the maintenance app network. We will email you with free leads as they come in. Thank you for your time."
+      redirect_to new_tradie_registration_path
       
-      if !params[:maintenance_request_id].blank?
-        
-        flash[:success] = "Thank you for joining the maintenance app network. Please login to continue to the maintenance request where you can create a quote."
-        redirect_to root_path(maintenance_request_id:params[:maintenance_request_id])
-      else
-        flash[:success] = "Thank you for joining the maintenance app network. We will email you with free leads as they come in. Thank you for your time."
-        redirect_to root_path
-      end 
       
     elsif params[:terms_and_conditions] == "false"
       @maintenance_request_id = params[:maintenance_request_id]
