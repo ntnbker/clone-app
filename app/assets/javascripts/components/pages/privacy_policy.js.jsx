3a849f03ddecfadef88c48382f142fdb135cc954
jeff <ntnbker@gmail.com>
2018-04-03 16:07:16 +0700

    add privacy policy page

---
+++ app/assets/javascripts/components/pages/privacy_policy.js.jsx
@@ -0,0 +1,200 @@
+var PrivacyPolicy = React.createClass({
+  render() {
+    return (
+      <div id="static-pages" className="privacy-policy">
+        <div className="scroll-bar">
+          <h4 className="text-center title">Privacy Policy</h4>
+          <p className="content level-1">
+            In this Privacy Policy, '<b>us</b>' '<b>we</b>' or '<b>our</b>' means Lab Developed Pty Ltd (ACN 608 599 293). We are committed to respecting your privacy. Our Privacy Policy sets out how we collect, use, store and disclose your personal information.
+          </p>
+          <p className="content level-1">
+            By providing personal information to us, you consent to our collection, use and disclosure of your personal information in accordance with this Privacy Policy and any other arrangements that apply between us. We may change our Privacy Policy from time to time by publishing changes to it on our website. We encourage you to check our website periodically to ensure that you are aware of our current Privacy Policy.
+          </p>
+          <p className="content level-1">
+            Personal information includes information or an opinion about an individual that is reasonably identifiable. For example, this may include your name, age, gender, postcode and contact details. It may also include financial information, including your credit card information.
+          </p>
+          <h5 className="content level-0">
+            1. What personal information do we collect?
+          </h5>
+          <p className="content level-1">
+            We may collect the following types of personal information:
+          </p>
+          <p className="content level-2">
+            name;
+          </p>
+          <p className="content level-2">
+            mailing or street address;
+          </p>
+          <p className="content level-2">
+            email address;
+          </p>
+          <p className="content level-2">
+            telephone number and other contact details;
+          </p>
+          <p className="content level-2">
+            age or date of birth;
+          </p>
+          <p className="content level-2">
+            credit card information;
+          </p>
+          <p className="content level-2">
+            Australian Business Number (if applicable);
+          </p>
+          <p className="content level-2">
+            your device ID, device type, geo-location information, computer and connection information, statistics on page views, traffic to and from the sites, ad data, IP address and standard web log information;
+          </p>
+          <p className="content level-2">
+            details of the products and services we have provided to you or that you have enquired about, including any additional information necessary to deliver those products and services and respond to your enquiries;
+          </p>
+          <p className="content level-2">
+            all information to do with any services provided or received on the platform;
+          </p>
+          <p className="content level-2">
+            any additional information relating to you that you provide to us directly through our website or app or indirectly through your use of our website or app or online presence or through other websites or accounts from which you permit us to collect information;
+          </p>
+          <p className="content level-2">
+            information you provide to us through customer surveys; or
+          </p>
+          <p className="content level-2">
+            any other personal information that may be required in order to facilitate your dealings with us.
+          </p>
+          <p className="content level-1">
+            We may collect these types of personal information either directly from you, or from third parties. We may collect this information when you:
+          </p>
+          <p className="content level-2">
+            register on our website or app;
+          </p>
+          <p className="content level-2">
+            communicate with us through correspondence, chats, email, or when you share information with us from other social applications, services or websites;
+          </p>
+          <p className="content level-2">
+            interact with our sites, services, content and advertising; or
+          </p>
+          <p className="content level-2">
+            invest in our business or enquire as to a potential purchase in our business.
+          </p>
+          <p className="content level-1">
+            In addition, when you apply for a job or position with us we may collect certain information from you (including your name, contact details, working history and relevant records checks) from any recruitment consultant, your previous employers and others who may be able to provide information to us to assist in our decision on whether or not to make you an offer of employment or engage you under a contract. This Privacy Policy does not apply to acts and practices in relation to employee records of our current and former employees, which are exempt from the Privacy Act.
+          </p>
+          <h5 className="content level-0">
+            2. Why do we collect, use and disclose personal information?
+          </h5>
+          <p className="content level-1">
+            We may collect, hold, use and disclose your personal information for the following purposes:
+          </p>
+          <p className="content level-2">
+            to enable you to access and use our website, services and app;
+          </p>
+          <p className="content level-2">
+            to operate, protect, improve and optimise our website, services and app, business and our users’ experience, such as to perform analytics, conduct research and for advertising and marketing;
+          </p>
+          <p className="content level-2">
+            to send you service, support and administrative messages, reminders, technical notices, updates, security alerts, and information requested by you;
+          </p>
+          <p className="content level-2">
+            to send you marketing and promotional messages and other information that may be of interest to you, including information sent by, or on behalf of, our business partners that we think you may find interesting;
+          </p>
+          <p className="content level-2">
+            to administer rewards, surveys, contests, or other promotional activities or events sponsored or managed by us or our business partners;
+          </p>
+          <p className="content level-2">
+            to comply with our legal obligations, resolve any disputes that we may have with any of our users, and enforce our agreements with third parties; and
+          </p>
+          <p className="content level-2">
+            to consider your employment application.
+          </p>
+          <p className="content level-1">
+            We may also disclose your personal information to a trusted third party who also holds other information about you. This third party may combine that information in order to enable it and us to develop anonymised consumer insights so that we can better understand your preferences and interests, personalise your experience and enhance the products and services that you receive.
+          </p>
+          <h5 className="content level-0">
+            3. Do we use your personal information for direct marketing?
+          </h5>
+          <p className="content level-1">
+            We and/or our carefully selected business partners may send you direct marketing communications and information about our services. This may take the form of emails, SMS, mail or other forms of communication, in accordance with the Spam Act and the Privacy Act. You may opt-out of receiving marketing materials from us by contacting us using the details set out below or by using the opt-out facilities provided (eg an unsubscribe link).
+          </p>
+          <h5 className="content level-0">
+            4. To whom do we disclose your personal information?
+          </h5>
+          <p className="content level-1">
+            We may disclose personal information for the purposes described in this privacy policy to:
+          </p>
+          <p className="content level-2">
+            our employees and related bodies corporate;
+          </p>
+          <p className="content level-2">
+            third party suppliers and service providers (including providers for the operation of our websites and/or our business or in connection with providing our products and services to you) – however do not disclose Recruitment Information to these persons;
+          </p>
+          <p className="content level-2">
+            professional advisers, dealers and agents;
+          </p>
+          <p className="content level-2">
+            payment systems operators (eg merchants receiving card payments);
+          </p>
+          <p className="content level-2">
+            our existing or potential agents, business partners or partners;
+          </p>
+          <p className="content level-2">
+            our sponsors or promoters of any competition that we conduct via our services;
+          </p>
+          <p className="content level-2">
+            anyone to whom our assets or businesses (or any part of them) are transferred;
+          </p>
+          <p className="content level-2">
+            specific third parties authorised by you to receive information held by us; and/or
+          </p>
+          <p className="content level-2">
+            other persons, including government agencies, regulatory bodies and law enforcement agencies, or as required, authorised or permitted by law.
+          </p>
+          <h5 className="content level-0">
+            5. Using our website and cookies
+          </h5>
+          <p className="content level-3">
+            We may collect personal information about you when you use and access our website.
+          </p>
+          <p className="content level-3">
+            While we do not use browsing information to identify you personally, we may record certain information about your use of our website, such as which pages you visit, the time and date of your visit and the internet protocol address assigned to your computer.
+          </p>
+          <p className="content level-3">
+            We may also use 'cookies' or other similar tracking technologies on our website that help us track your website usage and remember your preferences. Cookies are small files that store information on your computer, TV, mobile phone or other device. They enable the entity that put the cookie on your device to recognise you across different websites, services, devices and/or browsing sessions. You can disable cookies through your internet browser but our websites may not work as intended for you if you do so.
+          </p>
+          <p className="content level-3">
+            We may also use cookies to enable us to collect data that may include personal information. For example, where a cookie is linked to your account, it will be considered personal information under the Privacy Act. We will handle any personal information collected by cookies in the same way that we handle all other personal information as described in this Privacy Policy.
+          </p>
+          <h5 className="content level-0">
+            6. Security
+          </h5>
+          <p className="content level-1">
+            We may hold your personal information in either electronic or hard copy form. We take reasonable steps to protect your personal information from misuse, interference and loss, as well as unauthorised access, modification or disclosure and we use a number of physical, administrative, personnel and technical measures to protect your personal information. However, we cannot guarantee the security of your personal information.
+          </p>
+          <h5 className="content level-0">
+            7. Links
+          </h5>
+          <p className="content level-1">
+            Our website may contain links to websites operated by third parties. Those links are provided for convenience and may not remain current or be maintained. Unless expressly stated otherwise, we are not responsible for the privacy practices of, or any content on, those linked websites, and have no control over or rights in those linked websites. The privacy policies that apply to those other websites may differ substantially from our Privacy Policy, so we encourage individuals to read them before using those websites.
+          </p>
+          <h5 className="content level-0">
+            8. Accessing or correcting your personal information
+          </h5>
+          <p className="content level-1">
+            You can access the personal information we hold about you by contacting us using contact us function. Sometimes, we may not be able to provide you with access to all of your personal information and, where this is the case, we will tell you why. We may also need to verify your identity when you request your personal information.
+          </p>
+          <p className="content level-1">
+            If you think that any personal information we hold about you is inaccurate, please contact us and we will take reasonable steps to ensure that it is corrected.
+          </p>
+          <h5 className="content level-0">
+            9. Making a complaint
+          </h5>
+          <p className="content level-1">
+            If you think we have breached the Privacy Act, or you wish to make a complaint about the way we have handled your personal information, you can contact us [include hyperlink]. Please include your name, email address and/or telephone number and clearly describe your complaint. We will acknowledge your complaint and respond to you regarding your complaint within a reasonable period of time. If you think that we have failed to resolve the complaint satisfactorily, we will provide you with information about the further steps you can take.
+          </p>
+          <h5 className="content level-0">
+            10. Contact Us
+          </h5>
+          <p className="content level-1">
+            For further information about our Privacy Policy or practices, or to access or correct your personal information, or make a complaint, please contact us using the details set out below:
+          </p>
+        </div>
+      </div>
+    )
+  }
+})
