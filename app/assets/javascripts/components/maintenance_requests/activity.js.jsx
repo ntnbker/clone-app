fc2815af0d1fdc28b4e7642ed37c1b33d47f41ce
Jeff the Killer <jeff@Jeffs-MBP.net.fpt>
2018-05-04 15:11:07 +0700

    change icon maintenance actions and change h5 title to h4

--- app/assets/javascripts/components/maintenance_requests/activity.js.jsx
+++ app/assets/javascripts/components/maintenance_requests/activity.js.jsx
@@ -42,7 +42,7 @@ var Activity = React.createClass({
 		return (
 			<div className="item box-shadow">
 				<div className="header action">
-					<h4 className="mr-title activity">Activity log</h4>
+					<h5 className="mr-title activity">Activity log</h5>
 					<i className={this.state.show ? "fa fa-angle-down" : "fa fa-angle-right"} aria-hidden="true" onClick={this.showActivity}></i>
 				</div>
 				<div className="content text-center activity-content" id="activity-content">
