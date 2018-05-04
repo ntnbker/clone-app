88645ec4154a985ed5b5f5646473dce658ae63c3
Jeff the Killer <jeff@Jeffs-MBP.net.fpt>
2018-05-04 17:07:02 +0700

    change style for action title

--- app/assets/javascripts/components/invoices/index.js.jsx
+++ app/assets/javascripts/components/invoices/index.js.jsx
@@ -161,7 +161,7 @@ var Invoices = React.createClass({
                       <span className="value">{trady.company_name}</span>
                     </div>
                     <div className="invoice-status">
-                      <span>Invoice Status: </span>
+                      <span className="key">Invoice Status: </span>
                       {
                         paid == false
                           ? active !== false
