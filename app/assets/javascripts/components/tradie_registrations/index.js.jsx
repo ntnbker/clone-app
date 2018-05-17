cc128edb1e55dde54920908f97f8c76c6a89d54f
Jeff <ntnbker@gmail.com>
2018-05-17 18:05:26 +0700

    add some style for license and insurance

--- app/assets/javascripts/components/tradie_registrations/index.js.jsx
+++ app/assets/javascripts/components/tradie_registrations/index.js.jsx
@@ -1,7 +1,7 @@
 var TradyRegistrationForm = React.createClass({
   getInitialState : function() {
     const {step} = this.props;
-    const level = ['terms-and-conditions', 'trady', 'trady-company', 'service'];
+    const level = ['terms-and-conditions', 'trady', 'trady-company', 'service', 'license', 'insurance'];
 
     return {
       errors: {},
@@ -34,6 +34,8 @@ var TradyRegistrationForm = React.createClass({
         {this.labelTitle(2, <p>User<br />Registration</p>, level > 1)}
         {this.labelTitle(3, <p>Company<br />Registration</p>, level > 2)}
         {this.labelTitle(4, <p>Services<br />Available</p>, level > 3)}
+        {this.labelTitle(5, <p>Upload<br />License</p>, level > 4)}
+        {this.labelTitle(6, <p>Upload<br />Insurance</p>, level > 5)}
       </div>
     )
   },
@@ -50,6 +52,10 @@ var TradyRegistrationForm = React.createClass({
         return <h5 className="step-title text-center">Company Registration</h5>
       case 'service':
         return <h5 className="step-title text-center">Services Registration</h5>
+      case 'license':
+      return <h5 className="step-title text-center">Upload License</h5>
+      case 'insurance':
+      return <h5 className="step-title text-center">Upload Insurance</h5>
     }
   },
 
@@ -65,6 +71,10 @@ var TradyRegistrationForm = React.createClass({
         return <NewTradyCompany {...this.props} />;
       case 'service':
         return <ServiceList {...this.props} />;
+      case 'license':
+      return <TradyLicenseAndInsurance {...this.props} />;
+      case 'service':
+      return <TradyLicenseAndInsurance {...this.props} />;
     }
   },
 
@@ -82,3 +92,75 @@ var TradyRegistrationForm = React.createClass({
     )
   }
 });
+
+var TradyLicenseAndInsurance = React.createClass({
+  getInitialState() {
+    return {
+      text: '',
+      gallery: null,
+    }
+  },
+
+  uploadImage(images, callback) {
+    if (!images || !images.length) {
+      return callback('Image Not Found');
+    }
+
+    this.setState({gallery: images[0]});
+    return callback();
+  },
+
+  onSubmit(e) {
+    e.preventDefault();
+    const self = this;
+    const {gallery} = this.state;
+    // if (!gallery) return;
+    
+    $.ajax({
+      type: 'POST',
+      url: self.props.isLicense ? '/licenses' : '/insurances',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('X-CSRF-Token', self.props.authenticity_token);
+      },
+      data: {
+        licenses: JSON.stringify(gallery || {}),
+        trady_id: self.props.trady_id
+      },
+      success(res) {
+
+      },
+      error(err) {
+
+      }
+    });
+  },
+
+  render() {
+    const {gallery} = this.state;
+    return (
+      <div>
+        <form role="form" className="form-horizontal" id="upload-license-insurance" onSubmit={this.onSubmit} >
+          <div className="upload-description">
+            {this.state.text}
+          </div>
+          { gallery &&
+              <div className="image text-center">
+                <img id="avatar" src={gallery.image_url} alt="Avatar Image"/>
+              </div>
+          }
+          <div className="image-upload text-center">
+            <ModalImageUpload
+              uploadImage={this.uploadImage}
+              gallery={gallery && [gallery] || []}
+              text="Add/Change Photo"
+              className="btn button-primary option-button"
+            />
+          </div>
+          <div className="text-center">
+            <button type="submit" className="submit">submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+})
