4cbad04316647bb7c4797d39ff501d807642269d
ronval <ronald.m.valencia@gmail.com>
2018-05-23 17:55:04 +1000

    remove syntax error

--- app/models/license.rb
+++ app/models/license.rb
@@ -2,7 +2,7 @@ class License < ApplicationRecord
   include LicenseImageUploader::Attachment.new(:image) # adds an `image` virtual attribute
   belongs_to :trady
   validates :image, presence: { message: "Please pick a file." }, if: :perform_presence_validation
-  validates :license_number, :license_type, presence: true,, if: :perform_presence_validation
+  validates :license_number, :license_type, presence: true, if: :perform_presence_validation
 
   attr_accessor :perform_presence_validation
   attr_accessor :maintenance_request_id
