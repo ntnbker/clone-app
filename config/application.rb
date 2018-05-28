1508ad041c97a319dcf6456970056cdf2be9b0ad
ronval <ronald.m.valencia@gmail.com>
2018-05-28 18:06:34 +1000

    added setting for local Cors to application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,3 +15,13 @@ module Realestate
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
+
+
+config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    end
+  end
