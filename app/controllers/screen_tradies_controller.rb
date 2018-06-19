44c678b052c81ae967d9ac5a112ac45673373900
ronval <ronald.m.valencia@gmail.com>
2018-06-19 20:20:20 +1000

    added emails and more logic for screening tradies

--- app/controllers/screen_tradies_controller.rb
+++ app/controllers/screen_tradies_controller.rb
@@ -10,19 +10,6 @@ class ScreenTradiesController < ApplicationController
 
   def screen
     
-
-    # if both license and insurnace are yes then update both to true and also add trady registratino to "Complete" 
-    # Then send them an email letting them know they have been approved
-
-    # if the license is good but not the insurance update the license to true , dont update insurance and update trady registratino to "Incomplete"
-    # Then send them an email tellign them we need the insurance
-
-    # if the insurance is good but not the license update the insurnace to true, dont update license and update trady registratino to "Incomplete"
-    # Then send them an email tellign them we need the license
-
-
-    # do the skills here 
-    
     trady = Trady.find_by(id:params[:id])
     if params[:skill]
       new_skills= params[:skill][:skill]
@@ -43,53 +30,62 @@ class ScreenTradiesController < ApplicationController
       end 
       
       
-      if params[:insured] == true && params[:licensed] == true 
+
+      if params[:insured] == "true" && params[:licensed] == "true" 
         trady.insurance.update_attribute(:insured, true)
         trady.license.update_attribute(:licensed, true)
         trady.update_attribute(:registration_status, "Complete")
         #send email to trady saying they have been approved
-      elsif params[:insured] == false && params[:licensed] == true 
+        TradyApprovedEmailWorker.perform_async(trady.id)
+      elsif params[:insured] == "false" && params[:licensed] == "true" 
         trady.license.update_attribute(:licensed, true)
+        if trady.insurance == nil
+          #do nothing
+        else
+          trady.insurance.update_attribute(:insured, false)
+        end
+        
         trady.update_attribute(:registration_status, "Incomplete")
         #send email letting them know that we are missing their insurance
-      elsif params[:insured] == true && params[:licensed] == false
-        trady.insured.update_attribute(:insured, true)
-        trady.update_attribute(:registration_status, "Incomplete")
-        #send email letting them know that we are missing their license
-      elsif params[:insured] == false && params[:licensed] == false 
+        TradyMissingInsuranceEmailWorker.perform_async(trady.id)
+      elsif params[:insured] == "true" && params[:licensed] == "false"
+        trady.insurance.update_attribute(:insured, true)
+        
         if trady.license == nil 
           #do nothing
         else
           trady.license.update_attribute(:licensed, false)
-
+        end
+        
+        trady.update_attribute(:registration_status, "Incomplete")
+        TradyMissingLicenseEmailWorker.perform_async(trady.id)
+        #send email letting them know that we are missing their license
+      elsif params[:insured] == "false" && params[:licensed] == "false" 
+        if trady.license == nil 
+          #do nothing
+        else
+          trady.license.update_attribute(:licensed, false)
         end 
 
         if trady.insurance == nil
           #do nothing
         else
-          trady.insured.update_attribute(:insured, true)
-          
+          trady.insurance.update_attribute(:insured, false)
         end
+        
         trady.update_attribute(:registration_status, "Incomplete")
-        #send and email saying we are missing information. 
+        TradyMissingDocumentsEmailWorker.perform_async(trady.id)
       end 
         
 
         
-
-
-
-
-
-
-      #redirect_to new_tradie_term_agreement_path(maintenance_request_id:params[:maintenance_request_id], trady_company_id:params[:trady_company_id], trady_id:params[:trady_id])
-      flash[:success] = "Thank you for screening this tradie, he will receive an email to let them know what they need/can do now "
+      flash[:success] = "Thank you for screening this tradie, they will receive an email to let them know what they need/can do now "
       redirect_to screen_tradies_path
     else
       
       
       respond_to do |format|
-        format.json {render :json=>{errors:"Please choose at least one service from the list below, thank you."}}
+        format.json {render :json=>{errors:"Please choose at least one service from the list, thank you."}}
         format.html{render :edit_services}
       end
     end 
