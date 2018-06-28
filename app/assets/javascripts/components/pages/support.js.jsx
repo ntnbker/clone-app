aa840807d4b3d99fbf0851a17f4f41d45fbc0318
Jeff <ntnbker@gmail.com>
2018-06-28 18:19:02 +0700

    fix error

--- app/assets/javascripts/components/pages/support.js.jsx
+++ app/assets/javascripts/components/pages/support.js.jsx
@@ -24,7 +24,7 @@ const Support = React.createClass({
     )
   },
 
-  removeError: function({ tarsget: { id } }) {
+  removeError: function({ target: { id } }) {
     this.setState({
       errors: {
         ...this.state.errors,
