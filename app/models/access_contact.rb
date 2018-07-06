596bdc5b43c0b3696fb9188b96cd40da0e3166b6
ronval <ronald.m.valencia@gmail.com>
2018-07-06 20:01:38 +1000

    added code

--- app/models/access_contact.rb
+++ app/models/access_contact.rb
@@ -1,10 +1,10 @@
 class AccessContact < ApplicationRecord
   belongs_to :maintenance_request, inverse_of: :access_contacts
 
-  validates_presence_of :relation,:name,:email, :mobile, if: :turn_off_validitions
+  validates_presence_of :relation,:name,:email, :mobile, if: :turn_off_validations
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates :mobile,:presence => true, :numericality => true, :length => { :maximum => 10 }
 
-  attr_accessor :turn_off_validitions
+  attr_accessor :turn_off_validations
 end 
 
