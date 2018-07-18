55ad42eac7f9f486d8ae4728ebd086c5a98ae63d
ronval <ronald.m.valencia@gmail.com>
2018-07-18 17:11:56 +1000

    added action status to returned mr fr search

--- app/controllers/search_maintenance_requests_controller.rb
+++ app/controllers/search_maintenance_requests_controller.rb
@@ -5,7 +5,7 @@ class SearchMaintenanceRequestsController < ApplicationController
   def search_maintenance_request
     user = current_user
     @link = ''
-
+binding.pry
     if params[:query]
       if params[:search_archived]== "true" 
         if current_user.agency_admin && current_user.logged_in_as("AgencyAdmin")
@@ -20,11 +20,11 @@ class SearchMaintenanceRequestsController < ApplicationController
       elsif params[:search_archived] == "false"
         if current_user.agency_admin && current_user.logged_in_as("AgencyAdmin")
           @link = '/agency_admin_maintenance_requests'
-          m = MaintenanceRequest.search(params[:query],includes: [:property] ,where: {agency_admin_id: current_user.agency_admin.id})
-          @maintenance_requests = m.as_json(:include=>{:property=>{}},methods: :get_image_urls)
+          m = MaintenanceRequest.search(params[:query],includes: [:property, :action_status] ,where: {agency_admin_id: current_user.agency_admin.id})
+          @maintenance_requests = m.as_json(:include=>{:property=>{},:action_status=>{}},methods: :get_image_urls)
         elsif current_user.agent && current_user.logged_in_as("Agent")
-          m = MaintenanceRequest.search(params[:query],includes: [:property] ,where: {agent_id: current_user.agent.id})
-          @maintenance_requests = m.as_json(:include=>{:property=>{}},methods: :get_image_urls)
+          m = MaintenanceRequest.search(params[:query],includes: [:property, :action_status] ,where: {agent_id: current_user.agent.id})
+          @maintenance_requests = m.as_json(:include=>{:property=>{},:action_status=>{}},methods: :get_image_urls)
           @link = '/agent_maintenance_requests'
         end
       end 
